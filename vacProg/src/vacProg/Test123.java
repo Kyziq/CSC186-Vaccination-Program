0641c9622a8cb840860340a8063c84354ec6b4af
lildibbb <adib.asyraaf760@gmail.com>
2021-07-13 22:02:35 +0800

    halo

--- vacProg/src/vacProg/Test123.java
+++ vacProg/src/vacProg/Test123.java
@@ -1,5 +1,25 @@
 package vacProg;
+import java.util.*;
 
-public class Test123 {
-
+public class Test123
+{
+	public static void main (String args[]) {
+		   Scanner identity = new Scanner(System.in);
+		   
+		   String name;
+		   int ic;
+		   String vac;
+		   
+		   System.out.println("Please enter your name : ");
+		   name = identity.next();
+		   
+		   System.out.println("Please enter your identification card(IC) number : ");
+		   ic = identity.nextInt();
+		   
+		   System.out.println("Have you been vaccinated before ? : ");
+		   vac = identity.next();
+	}
+   
+   
 }
+
